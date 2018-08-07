f794f4c51ef0f76bfb4c6e73d9010e544896d8e5
Felisia Martini <felisia_martini@hotmail.com>
2018-08-07 20:34:38 +0100

    Added class for permutation palindrome problem

---
+++ DataStructureTests/PalindromePermutationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DataStructures;
+using Xunit;
+
+namespace DataStructureTests
+{
+    
+    public class PalindromePermutationTests
+    {
+        [Fact]
+        public void Should_ReturnTrue_When_AnyPermutationIsAPalindrome()
+        {
+            // [arrange]
+            var input = "tact coa";
+            var underTest = new PalindromePermutation();
+
+            // [act]
+            var result = underTest.HasPermutationsThatArePalindrome(input);
+
+            // [assert]
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_ReturnFalse_When_NoPermutationIsAPalindrome()
+        {
+            // [arrange]
+            var input = "no permutations of this are a palindrome";
+            var underTest = new PalindromePermutation();
+
+            // [act]
+            var result = underTest.HasPermutationsThatArePalindrome(input);
+
+            // [assert]
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Should_ReturnFalse_When_StringHasNoLetters()
+        {
+            // [arrange]
+            var input = " ";
+            var underTest = new PalindromePermutation();
+
+            // [act]
+            var result = underTest.HasPermutationsThatArePalindrome(input);
+
+            // [assert]
+            Assert.False(result);
+        }
+    }
+}
---
+++ DataStructures/PalindromePermutation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Given a string, write a function to check if it is a permutation of a palindrome.
+    /// A palindrome is a word or phrase that is the same forwards and backwards.
+    /// A permutation is a rearrangement of letters.
+    /// The palindrome does not need to be limited to just dictionary words.
+    /// </summary>
+    public class PalindromePermutation
+    {
+        public bool HasPermutationsThatArePalindrome(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            var cleanInput = input.ToLowerInvariant().Replace(" ", "");
+
+            var charMap = CreateCharactersMap(cleanInput);
+
+            var evenCharsSum = 0;
+            var oddCharsSum = 0;
+            CountEvenAndOddCharSum(charMap, ref evenCharsSum, ref oddCharsSum);
+
+            if (IsEvenNumber(cleanInput.Length))
+            {
+                if (oddCharsSum > 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            if (oddCharsSum > 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void CountEvenAndOddCharSum(
+            Dictionary<char, int> charMap,
+            ref int evenCharsSum,
+            ref int oddCharsSum)
+        {
+            foreach (var keyValuePair in charMap)
+            {
+                if (IsEvenNumber(keyValuePair.Value))
+                {
+                    evenCharsSum++;
+                }
+                else
+                {
+                    oddCharsSum++;
+                }
+            }
+        }
+
+        private Dictionary<char, int> CreateCharactersMap(string inputString)
+        {
+            var charMap = new Dictionary<char, int>();
+
+            foreach (var c in inputString)
+            {
+                if (charMap.ContainsKey(c))
+                {
+                    charMap[c]++;
+                }
+                else
+                {
+                    charMap.Add(c, 1);
+                }
+            }
+            return charMap;
+        }
+
+        private bool IsEvenNumber(int input)
+        {
+            return input % 2 == 0;
+        }
+    }
+}
